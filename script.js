12ed9e62189fa9f3815e3cf9fc4d0082637e4d4f
Lordluna67 <Pikachumastered@gmail.com>
2025-11-17 17:06:24 -0800

    Update script.js
    Additional style stuff

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    padding: 50px;
+    background-color: #f0f8ff;
+}
 
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    margin-top: 20px;
+    cursor: pointer;
+}
+
+#question-container {
+    margin-top: 30px;
+}
