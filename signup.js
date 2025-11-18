d88bec89f8522b4eebbc1e9c68e9aa4e8ea9dbaa
Lordluna67 <Pikachumastered@gmail.com>
2025-11-17 19:23:29 -0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -5,18 +5,18 @@ registerBtn.addEventListener("click", () => {
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
 
-    if (!email || !password) {
+    if(!email || !password){
         message.textContent = "Please enter both email and password.";
         return;
     }
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then(() => {
-            // Initialize tokens and daily spin
+            // initialize user stats
             localStorage.setItem(email + "_tokens", 500);
             localStorage.setItem(email + "_dailySpin", "true");
 
-            // Redirect to stats page
+            // redirect to stats page
             window.location.href = "stats.html";
         })
         .catch(err => {
