d38c710bca96260a69b430e3ef618484ec0006b8
Lordluna67 <Pikachumastered@gmail.com>
2025-11-17 20:36:13 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,26 @@
-const loginBtn = document.getElementById("login-btn");
-const message = document.getElementById("message");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-loginBtn.addEventListener("click", () => {
+const firebaseConfig = {
+    apiKey: "AIzaSyDpYoktMVRAeNm6TU-y54Ckmdkmfei_iRA",
+    authDomain: "blacket-game.firebaseapp.com",
+    projectId: "blacket-game",
+    storageBucket: "blacket-game.firebasestorage.app",
+    messagingSenderId: "82935109494",
+    appId: "1:82935109494:web:48d1cdf9cf89973ead8c57"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById("login-btn").addEventListener("click", () => {
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    signInWithEmailAndPassword(auth, email, password)
         .then(() => {
+            localStorage.setItem("currentUser", email);
             window.location.href = "stats.html";
         })
-        .catch(err => message.textContent = err.message);
+        .catch(err => document.getElementById("message").textContent = err.message);
 });
